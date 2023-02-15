2e4938dfe63f967f684537f72556ea0d0baf293d
Prottoss <prottoss@hotmail.co.uk>
2023-02-15 14:48:48 +0000

    cleanup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,10 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from "@capacitor/camera";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Filesystem } from '@capacitor/filesystem';
-import { Directory, FileInfo } from '@capacitor/filesystem/dist/esm/definitions';
-import { readBlobAsBase64 } from '@capacitor/core/types/core-plugins';
+import { Directory } from '@capacitor/filesystem/dist/esm/definitions';
 import { LoadingController, Platform } from '@ionic/angular';
-import { Form } from '@angular/forms';
 import { finalize } from 'rxjs';
 
 const IMAGE_DIR = "stored-images";
@@ -30,8 +28,6 @@ export class HomePage implements OnInit {
   token = "Token b52e5e9674fcb05988b8f207453578f42ac131b6";
   result: any;
 
-
-
   constructor(private http: HttpClient, private platform: Platform, private loadingCtrl: LoadingController){}
 
   async ngOnInit(){
@@ -52,7 +48,7 @@ export class HomePage implements OnInit {
       path: IMAGE_DIR
     }).then(result => {
 
-      console.log("HERE: ", result);
+      //console.log("HERE: ", result);
       this.loadFileData(result.files);
 
     }, async err => {
@@ -69,8 +65,6 @@ export class HomePage implements OnInit {
   async loadFileData(fileNames: any[]){
     for (let f of fileNames){
       const filePath = `${IMAGE_DIR}/${f.name}`;
-      console.log("HERE2: ",f);
-      
 
       const readFile = await Filesystem.readFile({
         directory: Directory.Data,
@@ -82,7 +76,7 @@ export class HomePage implements OnInit {
         path: filePath,
         data: `data:image/jpeg;base64,${readFile.data}`
       })
-      console.log("READ: ",readFile.data);
+      //console.log("READ: ",readFile.data);
     }
   }
 
@@ -93,9 +87,6 @@ export class HomePage implements OnInit {
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera
     });
-    //console.log("image: ", image.dataUrl);
-
-    //this.uploadData(image);
 
     if (image){
       this.saveImage(image);
@@ -114,92 +105,79 @@ export class HomePage implements OnInit {
       path: `${IMAGE_DIR}/${fileName}`,
       data: base64Data
     });
-    console.log("saved: ", savedFile);
+    //console.log("saved: ", savedFile);
     this.loadFiles();
   }
 
-  async sendImage(file: userImage)
+  async sendImage(image: userImage)
   {
-    const response = await fetch(file.data);
-    console.log("READ3",response);
-    const blob = await response.blob();
-    //console.log("READ4",blob);
-    const formData = new FormData();
-    formData.append("upload",file.data);
-    formData.append("regions","ie");
-    console.log("UPLOADRDY: ",file.data);
+    const loading = await this.loadingCtrl.create({
+      message: "Uploading...",
+    });
+    await loading.present();
+  
+    console.log("IMAGEUPLOAD",image.data);
     
-    //this.uploadData(formData);
-    this.uploadData(file);
+    const headers = new HttpHeaders()
+        .set('content-type', 'application/json')
+        .set("Authorization", this.token);
+  
+      const body = {
+        "regions": ["ie","gb"],
+        "upload": image.data
+      }
+  
+      this.http.post(this.apiUrl,body,{headers:headers}).pipe(
+        finalize(() => {
+          loading.dismiss();
+        })
+      ).subscribe((res:any)=>{
+        console.log(res.results[0]["plate"]);
+        this.result = res.results[0]["plate"];
+      }); 
+    // const response = await fetch(file.data);
+    // console.log("READ3",response);
+    // const blob = await response.blob();
+    // const formData = new FormData();
+    // formData.append("upload",file.data);
+    // formData.append("regions","ie");
+    // console.log("UPLOADRDY: ",file.data);
+
+    //this.uploadData(file);
+  }
+
+  async deleteImage(file:userImage)
+  {
+    await Filesystem.deleteFile({
+      directory: Directory.Data,
+      path: file.path
+    });
+    this.loadFiles();
   }
 
 //Helper functions
-async readAsBase64(photo: Photo){
-  if(this.platform.is("hybrid")){
-    const file = await Filesystem.readFile({
-      path: photo.path as string
-    });
+  async readAsBase64(photo: Photo){
+    if(this.platform.is("hybrid")){
+      const file = await Filesystem.readFile({
+        path: photo.path as string
+      });
 
-    return file.data;
-  }
-  else{
-    const response = await fetch(photo.webPath!);
-    const blob = await response.blob();
-    return await this.convertBlobToBase64(blob) as string;
-  }
-}
-
-convertBlobToBase64 = (blob:Blob) => new Promise((resolve,reject) =>{
-  const reader = new FileReader;
-  reader.onerror = reject;
-  reader.onload = () =>{
-    resolve(reader.result);
-  };
-  reader.readAsDataURL(blob);
-});
-
-async uploadData(image: any){
-  const loading = await this.loadingCtrl.create({
-    message: "Uploading...",
-  });
-  await loading.present();
-
-  console.log("IMAGEUPLOAD",image.data);
-  
-  const headers = new HttpHeaders()
-      .set('content-type', 'application/json')
-      //.set("Access-Control-Allow-Origin","*")
-      //.set("Access-Control-Request-Method","method")
-      .set("Authorization", this.token);
-
-    const body = {
-      "regions": ["ie","gb"],
-      "upload": image.data
+      return file.data;
     }
+    else{
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      return await this.convertBlobToBase64(blob) as string;
+    }
+  }
 
-    // const body = new FormData();
-    // body.append("upload",image);
-    // body.append("regions","ie,gb");
-
-    this.http.post(this.apiUrl,body,{headers:headers}).pipe(
-      finalize(() => {
-        loading.dismiss();
-      })
-    ).subscribe((res:any)=>{
-      console.log(res.results[0]["plate"]);
-      this.result = res.results[0]["plate"];
-    }); 
-
-
-}
-
-async deleteImage(file:userImage)
-{
-  await Filesystem.deleteFile({
-    directory: Directory.Data,
-    path: file.path
+  convertBlobToBase64 = (blob:Blob) => new Promise((resolve,reject) =>{
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () =>{
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
   });
-  this.loadFiles();
 
-}
 }
